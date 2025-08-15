81ab097a35fb562776ebb0bdf02b96e6c3a618bb
Dmytro Momot <mail@dmomot.com>
2025-08-15 12:18:16 +0700

    refactor(encapsulation): improve internal type visibility
    - Move responseWriter to a separate file
    - Make RouteParams type private (renamed to routeParams)
    - Hide internal response writer tracking methods

---
+++ response_writer.go
@@ -0,0 +1,35 @@
+package gokit
+
+import "net/http"
+
+// responseWriter wraps http.ResponseWriter to track if response was written.
+type responseWriter struct {
+	http.ResponseWriter
+	written bool
+	status  int
+}
+
+func (w *responseWriter) WriteHeader(status int) {
+	if !w.written {
+		w.status = status
+		w.written = true
+		w.ResponseWriter.WriteHeader(status)
+	}
+}
+
+func (w *responseWriter) Write(b []byte) (int, error) {
+	if !w.written {
+		w.WriteHeader(http.StatusOK)
+	}
+	return w.ResponseWriter.Write(b)
+}
+
+// Written returns true if the response has been written.
+func (w *responseWriter) Written() bool {
+	return w.written
+}
+
+// Status returns the HTTP status code of the response.
+func (w *responseWriter) Status() int {
+	return w.status
+}
